74ef5b818b8b18c8ecf5d5617f3b5b4a55bff050
donce <donce71@gmail.com>
2018-09-19 13:11:23 +0300

    subStates

--- src/mybot_control/script/state.py
+++ src/mybot_control/script/state.py
@@ -29,7 +29,9 @@ class Foo(smach.State):
 # define state Bar
 class Bar(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome1','outcome2'], input_keys=['bar_counter_in'],output_keys=['bar_counter_out'])
+        smach.State.__init__(self, outcomes=['outcome1'],
+                                   input_keys=['bar_counter_in'],
+                                   output_keys=['bar_counter_out'])
         self.counter = 0
 
     def execute(self, userdata):
@@ -41,12 +43,12 @@ class Bar(smach.State):
             return 'outcome1'
             self.counter = 0
         else: 
-            return 'outcome2'
+            return 'outcome1'
 
         # define state Bar
 class Work(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+        smach.State.__init__(self, outcomes=['outcome3','outcomeERROR'])
         self.counter = 0
 
     def execute(self, userdata):
@@ -54,9 +56,18 @@ class Work(smach.State):
         if self.counter < 5:
             self.counter += 1
             time.sleep(2) 
-            return 'outcome1'
+            return 'outcome3'
         else:
-            return 'outcome2'
+            return 'outcomeERROR'
+
+ # State Bas
+class SoneSUB(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome3'])
+      
+    def execute(self, userdata):
+        time.sleep(4) 
+        return 'outcome3'
         
 
 
@@ -65,33 +76,48 @@ def main():
     rospy.init_node('smach_example_state_machine')
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['outcome4'])
-    sm.userdata.sm_counter = 0
+    sm_top = smach.StateMachine(outcomes=['outcome5'])
+    sm_top.userdata.sm_counter = 0
 
     # Open the container
-    with sm:
+    with sm_top:
         # Add states to the container
-        smach.StateMachine.add('FOO', Foo(), 
+        smach.StateMachine.add('WORK', Work(), 
+                               transitions={'outcome3':'SUB',
+                                        'outcomeERROR':'SONE_SUB'})   
+
+        smach.StateMachine.add('SONE_SUB', SoneSUB(), 
+                               transitions={'outcome3':'outcome5'})   
+
+
+        sm_sub=smach.StateMachine(outcomes=['outcome4','outcome7'])
+        sm_sub.userdata.sm_sub_counter = 0
+
+        #open sub container
+        with sm_sub:
+            # Add states to the container
+            smach.StateMachine.add('FOO', Foo(), 
                                transitions={'outcome1':'BAR', 
                                             'outcome2':'outcome4'},
-                               remapping={'foo_counter_in':'sm_counter',
-                                          'foo_counter_out':'sm_counter'})
-        smach.StateMachine.add('BAR', Bar(), 
-                               transitions={'outcome1':'FOO', 
-                                            'outcome2':'WORK'},
-                                remapping={'bar_counter_in':'sm_counter',
-                                           'bar_counter_out':'sm_counter'})
-        smach.StateMachine.add('WORK', Work(), 
-                               transitions={'outcome1':'BAR',
-                                            'outcome2':'FOO'})                        
+                               remapping={'foo_counter_in':'sm_sub_counter',
+                                          'foo_counter_out':'sm_sub_counter'})
+
+            smach.StateMachine.add('BAR', Bar(), 
+                               transitions={'outcome1':'FOO'},
+                                remapping={'bar_counter_in':'sm_sub_counter',
+                                           'bar_counter_out':'sm_sub_counter'})    
+
+        smach.StateMachine.add('SUB',sm_sub,
+                                transitions={'outcome4':'outcome5',
+                                             'outcome7':'WORK'})                
                                
 
    # Create and start the introspection server
-    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    sis = smach_ros.IntrospectionServer('server_name', sm_top, '/SM_ROOT')
     sis.start()
 
     # Execute SMACH plan
-    outcome = sm.execute()
+    outcome = sm_top.execute()
 
     # Wait for ctrl-c to stop the application
     #rospy.spin()
