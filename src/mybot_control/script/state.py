3d312e585c967cae6e4c372df125d699398ff042
donce <donce71@gmail.com>
2018-09-23 16:23:28 +0300

    State with Call_Back_states

--- src/mybot_control/script/state.py
+++ src/mybot_control/script/state.py
@@ -1,119 +1,68 @@
 #!/usr/bin/env python
 
-import roslib   #roslib.load_manifest('smach_tutorials')
 import rospy
 import smach
 import smach_ros
-import time
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from smach import CBState
+
+@smach.cb_interface(input_keys=[], output_keys=[], outcomes=['finished', 'failed'])
+def takeoff_cb(user_data):
+    rospy.loginfo('Taking Off')
+    takeoff_topic = rospy.Publisher('/cmd_vel',  Twist, queue_size=10)
+    rospy.sleep(1)
+    msg = Twist()
+    msg.linear.x = 0.1
+    msg.angular.z = 0
+    result = takeoff_topic.publish(msg)
+    if result == None:
+        return 'finished'
+    else:
+        return 'failed'
 
 
-# define state Foo
-class Foo(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome1','outcome2'],
-                                    input_keys=['foo_counter_in'],
-                                    output_keys=['foo_counter_out'])
-        self.counter = 0
-
-    def execute(self, userdata):
-        rospy.loginfo('Executing state FOO')
-        if self.counter < 10:
-            userdata.foo_counter_out = userdata.foo_counter_in + 1
-            self.counter += 1
-            time.sleep(5) 
-            return 'outcome1'
-        else:
-            return 'outcome1'
+@smach.cb_interface(input_keys=[], output_keys=[], outcomes=['finished', 'failed'])
+def land_cb(user_data):
+    rospy.loginfo('Landing')
+    takeoff_topic = rospy.Publisher('/cmd_vel',  Twist, queue_size=10)
+    rospy.sleep(5)
+    msg = Twist()
+    msg.linear.x = 0
+    msg.angular.z = 0
+    result = takeoff_topic.publish(msg)
+    if result == None:
+        return 'finished'
+    else:
+        return 'failed'
 
 
-# define state Bar
-class Bar(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome1', 'error'],
-                                   input_keys=['bar_counter_in'],
-                                   output_keys=['bar_counter_out'])
-        self.counter = 0
-
-    def execute(self, userdata):
-        rospy.loginfo('Executing state BAR')
-        userdata.bar_counter_out = userdata.bar_counter_in + 1
-        time.sleep(1) 
-        self.counter += 1
-        if self.counter < 10:
-            return 'outcome1'
-            self.counter = 0
-        else: 
-            return 'error'
-
-        # define state Bar
-class Work(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome3'])
-        self.counter = 0
-
-    def execute(self, userdata):
-        rospy.loginfo('Executing state Work')
-        if self.counter < 5:
-            self.counter += 1
-            time.sleep(2) 
-            return 'outcome3'
-        else:
-            return 'outcome3'
-     
-
-
-# main
 def main():
-    rospy.init_node('smach_example_state_machine')
+    rospy.init_node('drone_state_machine')
 
     # Create a SMACH state machine
-    sm_top = smach.StateMachine(outcomes=['END1'])  
-  
+    sm = smach.StateMachine(outcomes=['outcome'])
+    sm.userdata.lspeed = 0.5
+    sm.userdata.rspeed = 1.2
+
     # Open the container
-    with sm_top:
+    with sm:
         # Add states to the container
-        smach.StateMachine.add('WORK', Work(), 
-                               transitions={'outcome3':'CON'})   
+        smach.StateMachine.add('TAKEOFF', CBState(takeoff_cb),
+                               {'finished': 'LAND', 'failed': 'LAND'})
+        smach.StateMachine.add('LAND', CBState(land_cb),
+                               {'finished': 'outcome', 'failed': 'outcome'})
 
-        sm_con = smach.Concurrence(outcomes = ['outcome1', 'outcome2','ENDerror'],
-                                   default_outcome = 'outcome1',
-                                   outcome_map = {'outcome2':{'FOO':'outcome1',
-                                                              'BAR':'outcome1'},
-                                                   'ENDerror':{'BAR':'error'}})
 
-        sm_con.userdata.sm_sub_counter = 0        
-        sm_con.userdata.sm_sub_counter2 = 0                                
-                        
-
-        #open sub container
-        with sm_con:
-            # Add states to the container
-            smach.Concurrence.add('FOO', Foo(), 
-                                    remapping={'foo_counter_in':'sm_sub_counter',
-                                             'foo_counter_out':'sm_sub_counter'})
-
-            smach.Concurrence.add('BAR', Bar(), 
-                                remapping={'bar_counter_in':'sm_sub_counter2',
-                                           'bar_counter_out':'sm_sub_counter2'})    
-
-        smach.StateMachine.add('CON', sm_con,
-                                  transitions={'outcome1':'CON',
-                                               'outcome2':'CON',
-                                               'ENDerror':'END1'})             
-
-                                   
-
-   # Create and start the introspection server
-    sis = smach_ros.IntrospectionServer('server_name', sm_top, '/SM_ROOT')
+    # Create and start the introspection server
+    sis = smach_ros.IntrospectionServer('drone_server', sm, '/SM_DRONE')
     sis.start()
 
     # Execute SMACH plan
-    outcome = sm_top.execute()
+    outcome = sm.execute()
 
-    # Wait for ctrl-c to stop the application
-    #rospy.spin()
     sis.stop()
 
 if __name__ == '__main__':
     main()
-
